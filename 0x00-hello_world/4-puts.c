794eff4f8bf100adef045c103e1f2cb94c24687f
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-07 12:26:48 -0700

    Add 4-puts.c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #include <stdio.h>
-int main(void) { return puts("Programming is like building a multilingual puzzle"), 0; }
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
