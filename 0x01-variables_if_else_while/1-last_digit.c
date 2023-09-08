ae4d3ed423356386c2d4293653405405c421a1ba
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-08 08:51:43 -0700

    Fix trailing whitespaces in 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,25 +9,26 @@
  */
 int main(void)
 {
-int n;
+    int n;
 
-srand(time(0)); 
+    srand(time(0)); 
 
-n = rand() - RAND_MAX / 2; 
+    n = rand() - RAND_MAX / 2; 
 
-printf("%d is ", n); 
+    printf("%d is ", n); 
 
-if (n > 0)
-{
-printf("positive\n");
-}
-else if (n < 0)
-{
-printf("negative\n");
-}
-else
-{
-printf("zero\n");
-}
-return (0);
+    if (n > 0)
+    {
+        printf("positive\n");
+    }
+    else if (n < 0)
+    {
+        printf("negative\n");
+    }
+    else
+    {
+        printf("zero\n");
+    }
+
+    return (0);
 }
