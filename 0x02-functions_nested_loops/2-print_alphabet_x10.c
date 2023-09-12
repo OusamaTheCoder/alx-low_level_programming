63e22b2e18d7b6f13108d8891d68d3b5925bd1ab
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-12 16:14:30 -0700

    Prints the alphabet in lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,20 +5,20 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char letter;
+    int i;
+    char letter;
 
-	for (i = 0; i < 10; i++)
-	{
-		letter = 'a';
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
 
-		while (letter <= 'z')
-		{
-			_putchar(letter);
-			letter++;
-		}
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+            letter++;
+        }
 
-		_putchar('\n');
-	}
+        _putchar('\n');
+    }
 }
 
