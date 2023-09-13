21f89cf78b0ed03beaf9a001472e8d752764da1d
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-12 19:32:38 -0700

    times_table - Print the 9 times table starting from 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,28 @@
  */
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= 9; i++) // Loop for rows
+	{
+		for (j = 0; j <= 9; j++) // Loop for columns
+		{
+			product = i * j;
 
-            if (j == 0)
-                _putchar('0');
-            else
-                _putchar(',');
+			if (j == 0)
+				_putchar('0'); // Print the first number without comma
+			else
+				_putchar(','); // Print comma and space before other numbers
 
+			// Print the product with proper formatting
+			if (product < 10)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0'); // Print tens digit
 
-            if (product < 10)
-                _putchar(' ');
-            else
-                _putchar((product / 10) + '0');
-
-            _putchar((product % 10) + '0');
-        }
-        _putchar('\n');
-    }
+			_putchar((product % 10) + '0'); // Print ones digit
+		}
+		_putchar('\n'); // Move to the next row after each inner loop
+	}
 }
 
