15cae80a10c1eabee64921e47e5605920799f55e
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-12 19:43:29 -0700

    print_times_table - Print the n times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,39 +9,14 @@ void print_times_table(int n)
     if (n < 0 || n > 15)
         return;
 
-    for (int i = 0; i <= n; i++)
+    int i, j;
+
+    for (i = 0; i <= n; i++)
     {
-        for (int j = 0; j <= n; j++)
+        for (j = 0; j <= n; j++)
         {
-            int result = i * j;
 
-            if (j == 0)
-                _putchar('0');
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-
-                if (result < 10)
-                    _putchar(' ');
-                if (result < 100)
-                    _putchar(' ');
-
-                if (result >= 100)
-                {
-                    _putchar((result / 100) + '0');
-                    _putchar(((result / 10) % 10) + '0');
-                }
-                else if (result >= 10)
-                {
-                    _putchar(' ');
-                    _putchar((result / 10) + '0');
-                }
-
-                _putchar((result % 10) + '0');
-            }
-        }
-        _putchar('\n');
+	}
     }
 }
 
