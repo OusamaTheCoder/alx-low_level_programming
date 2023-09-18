d437a978b15c0bb6a0583ee7a9865649176ffbfd
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-18 14:14:28 -0700

    Complete C project on pointers, arrays, strings, and variable scope

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - Return the length of a string.
+ * @s: A pointer to a string (null-terminated character array).
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (s != NULL)
+	{
+		while (*s != '\0')
+		{
+			length++;
+			s++;
+		}
+	}
+
+	return (length);
+}
+
