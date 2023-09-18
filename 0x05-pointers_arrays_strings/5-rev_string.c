0b5fa28f560168bb381b0ece6358478b2633a87f
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-18 15:26:59 -0700

    Implement the rev_string function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,26 +7,26 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int start, end;
-    char temp;
+	int length = 0;
+	int start, end;
+	char temp;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    start = 0;
-    end = length - 1;
+	start = 0;
+	end = length - 1;
 
-    while (start < end)
-    {
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
 
-        start++;
-        end--;
-    }
+		start++;
+		end--;
+	}
 }
 
