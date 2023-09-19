9395b4bacb18e420c986007ce649d902243c5945
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-18 17:29:53 -0700

    Password Generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,34 +3,34 @@
 #include <time.h>
 #include <string.h>
 
-#define PASSWORD_LENGTH 12 
-
+#define PASSWORD_LENGTH 12
 
 char generateRandomChar(const char *charset, int charsetLength) {
     return charset[rand() % charsetLength];
 }
 
 int main() {
-    const char *uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const char *lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    const char *numbers = "0123456789";
+    const char uppercaseLetters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char lowercaseLetters[] = "abcdefghijklmnopqrstuvwxyz";
+    const char numbers[] = "0123456789";
 
-    const char *charset = strcat(strcat(uppercaseLetters, lowercaseLetters), numbers);
+    char charset[PASSWORD_LENGTH + 1];
+    strcpy(charset, uppercaseLetters);
+    strcat(charset, lowercaseLetters);
+    strcat(charset, numbers);
+
     int charsetLength = strlen(charset);
 
     char password[PASSWORD_LENGTH + 1];
 
-    
     srand((unsigned int)time(NULL));
 
-    
     for (int i = 0; i < PASSWORD_LENGTH; i++) {
         password[i] = generateRandomChar(charset, charsetLength);
     }
 
     password[PASSWORD_LENGTH] = '\0';
 
-    
     printf("%s\n", password);
 
     return 0;
