0627e094fbec1c2a9f7e48722d8312f9d5a42162
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-21 12:11:11 -0700

    Prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
+	else if (n >= 10)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+}
+
