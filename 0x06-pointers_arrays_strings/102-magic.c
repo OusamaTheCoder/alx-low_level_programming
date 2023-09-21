1c7f5b0f3096df11f6cca73652996ede56690b5e
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-21 12:25:01 -0700

    Entry point

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
     int a[5];
-    int *p;
 
     a[2] = 1024;
-    p = a + 2;
-    /* Add one line here */
-    *(a + 2) = 98;
+    *(a + 5) = 98;
     printf("a[2] = %d\n", a[2]);
     return (0);
 }
