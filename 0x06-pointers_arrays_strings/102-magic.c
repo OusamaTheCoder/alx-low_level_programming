356fc94392ad3d8c1381a00c0832b98437f62cce
Ousama Oujaber <ndnd15237@gmail.com>
2023-09-21 12:28:36 -0700

    Entry point

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,11 +2,16 @@
 
 int main(void)
 {
-    int a[5];
+    int a[] = {0, 1, 2, 3, 4};
+    int *p;
 
-    a[2] = 1024;
-    *(a + 5) = 98;
-    printf("a[2] = %d\n", a[2]);
+    p = a;
+
+    printf("a[2] = %d\n", a[2]);  /* This line should not be modified */
+
+    /* Add your code below this line */
+    *(p + 2) = 98;
+
     return (0);
 }
 
