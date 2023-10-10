35bd3f700fcce5d9bfab07e6970e1d4c64209d93
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-10 13:54:37 -0700

    Add program to print the name of the source file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the name of the source file.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
