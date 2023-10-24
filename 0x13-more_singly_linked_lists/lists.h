0c6b62f59caa1bf253a3d353eff4e41a5e73d820
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-24 09:44:13 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,56 +20,25 @@
  */
 typedef struct listint_s
 {
-	int n;              /* Integer value stored in the node */
-	struct listint_s *next; /* Pointer to the next node in the list */
-} listint_t;            /* Alias for the struct */
-
-/* Function prototypes for working with singly linked lists */
+	int n;
+	struct listint_s *next;
+}
+listint_t;
 
 size_t print_listint(const listint_t *h);
-/* Print all elements of a linked list. */
-
 size_t listint_len(const listint_t *h);
-/* Calculate the length (number of nodes) of a linked list. */
-
 listint_t *add_nodeint(listint_t **head, const int n);
-/* Add a new node with a given integer value to the beginning of the list. */
-
 listint_t *add_nodeint_end(listint_t **head, const int n);
-/* Add a new node with a given integer value to the end of the list. */
-
 void free_listint(listint_t *head);
-/* Free the memory allocated for a linked list. */
-
 void free_listint2(listint_t **head);
-/* Free the memory allocated for a linked list and set the head to NULL. */
-
 int pop_listint(listint_t **head);
-/* Remove the first node of the linked list and return its value. */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-/* Get the node at a specified index in the linked list. */
-
 int sum_listint(listint_t *head);
-/* Calculate the sum of all the integer values in the linked list. */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-/* Insert a new node with a given integer value at a specified index in the list. */
-
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-/* Delete a node at a specified index in the linked list. */
-
 listint_t *reverse_listint(listint_t **head);
-/* Reverse the linked list in place. */
-
 size_t print_listint_safe(const listint_t *head);
-/* Print all elements of a linked list safely, handling loops. */
-
 size_t free_listint_safe(listint_t **h);
-/* Free the memory of a linked list safely, handling loops. */
-
 listint_t *find_listint_loop(listint_t *head);
-/* Find the beginning of a loop in a linked list if one exists. */
 
 #endif
-
