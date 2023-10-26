1b15ab42ccdb300493ad90763eb490f060c1f9b3
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-26 09:00:33 -0700

    Converts a binary number to an unsigned integer

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Converts a binary string to an unsigned integer */
+unsigned int binary_to_uint(const char *b);
+
+/* Prints the binary representation of a number */
+void print_binary(unsigned long int n);
+
+/* Gets the value of a bit at a given index */
+int get_bit(unsigned long int n, unsigned int index);
+
+/* Sets the value of a bit to 1 at a given index */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* Sets the value of a bit to 0 at a given index */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/* Counts the number of bits that need to be flipped to transition from one
+ * number to another */
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+/* Checks the endianness of the system
+ * (0 for big endian, 1 for little endian) */
+int get_endianness(void);
+
+#endif /* MAIN_H */
+
