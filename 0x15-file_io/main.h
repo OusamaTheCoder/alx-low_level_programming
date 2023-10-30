d28908457ac4f6f96498f8eeec3f7dd46d45334b
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-30 08:59:30 -0700

    Upload files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* 0. Tread lightly, she is near */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* 1. Under the snow */
+int create_file(const char *filename, char *text_content);
+
+/* 2. Speak gently, she can hear */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* 3. cp */
+int cp(const char *file_from, const char *file_to);
+
+#endif /* MAIN_H */
+
