725721160f41a32d8f3e4143c4f6c1e7de387f10
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-30 10:00:43 -0700

    Display an error message and exit with the given status

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFSIZE 1024
+
+/**
+ * error_exit - Display an error message and exit with the given status.
+ * @status: The exit status.
+ * @format: The error message format.
+ */
+void error_exit(int status, const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	dprintf(STDERR_FILENO, format, args);
+	va_end(args);
+	exit(status);
+}
+
+/**
+ * main - Copy the content of one file to another.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFSIZE];
+	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to\n");
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	if (fd_to == -1)
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+
+	while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	if (bytes_read == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+
+	return (0);
+}
+
