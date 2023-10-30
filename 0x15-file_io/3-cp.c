3f6ec8ad3b39a2e22f2bd2a19557218af4ddb538
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-30 10:10:54 -0700

    Fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,66 +15,67 @@
  */
 int main(int argc, char *argv[])
 {
-    int from_fd, to_fd, bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
-    char *from_filename, *to_filename;
+	int from_fd, to_fd, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	char *from_filename, *to_filename;
 
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
-        return (97);
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		return (97);
+	}
+
+	from_filename = argv[1];
+	to_filename = argv[2];
+
+	from_fd = open(from_filename, O_RDONLY);
+	if (from_fd == -1)
+	{
+		dprintf(2, "Error: Can't read from %s\n", from_filename);
+		return (98);
     }
 
-    from_filename = argv[1];
-    to_filename = argv[2];
+	to_fd = open(to_filename, O_WRONLY | O_CREAT |
+			O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (to_fd == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", to_filename);
+		close(from_fd);
+		return (99);
+	}
 
-    from_fd = open(from_filename, O_RDONLY);
-    if (from_fd == -1)
+	while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(to_fd, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(2, "Error: Can't write to %s\n", to_filename);
+			close(from_fd);
+			close(to_fd);
+			return (99);
+		}
+	}
+
+	if (bytes_read == -1)
     {
-        dprintf(2, "Error: Can't read from %s\n", from_filename);
-        return (98);
-    }
+		dprintf(2, "Error: Can't read from %s\n", from_filename);
+		close(from_fd);
+		close(to_fd);
+		return (98);
+	}
 
-    to_fd = open(to_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (to_fd == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", to_filename);
-        close(from_fd);
-        return (99);
-    }
+	if (close(from_fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", from_fd);
+		return (100);
+	}
 
-    while ((bytes_read = read(from_fd, buffer, BUFFER_SIZE)) > 0)
-    {
-        bytes_written = write(to_fd, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-        {
-            dprintf(2, "Error: Can't write to %s\n", to_filename);
-            close(from_fd);
-            close(to_fd);
-            return (99);
-        }
-    }
+	if (close(to_fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", to_fd);
+		return (100);
+	}
 
-    if (bytes_read == -1)
-    {
-        dprintf(2, "Error: Can't read from %s\n", from_filename);
-        close(from_fd);
-        close(to_fd);
-        return (98);
-    }
-
-    if (close(from_fd) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", from_fd);
-        return (100);
-    }
-
-    if (close(to_fd) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", to_fd);
-        return (100);
-    }
-
-    return (0);
+	return (0);
 }
 
