c6964ab6b085af124a95ee6e034418cc29804ede
Ousama Oujaber <ndnd15237@gmail.com>
2023-12-07 17:47:00 -0800

    ah shit, here we go again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,39 +9,45 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *temp = *h;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int to_index = 0;
 
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 
-	if (idx == 0)
+	if (*h == NULL)
 	{
+		new_node->next = NULL;
 		new_node->prev = NULL;
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
 
-	for (unsigned int i = 0; i < idx - 1; i++)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (to_index < idx - 1)
 	{
 		if (temp == NULL)
-		{
-			free(new_node);
 			return (NULL);
-		}
 		temp = temp->next;
+		to_index++;
+	}
+
+	if (temp->next == NULL)
+	{
+		temp->next = new_node;
+		new_node->prev = temp;
+		new_node->next = NULL;
+		return (new_node);
 	}
 
 	new_node->next = temp->next;
-	new_node->prev = temp;
-	if (temp->next != NULL)
-		temp->next->prev = new_node;
 	temp->next = new_node;
-
+	new_node->prev = temp;
+	new_node->next->prev = new_node;
 	return (new_node);
 }
+
