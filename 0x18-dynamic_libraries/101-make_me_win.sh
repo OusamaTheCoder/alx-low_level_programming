0bcafcc8f4a67a800a82e85d5cd1762f8f988236
Ousama Oujaber <ndnd15237@gmail.com>
2023-12-11 17:53:05 -0800

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/OusamaTheCoder/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib_giga.so
+wget -P .. https://github.com/OusamaTheCoder/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lib_giga.so
 export LD_PRELOAD="$PWD/../lib_giga.so"
